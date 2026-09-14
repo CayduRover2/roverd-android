2b3517e9db847d96606eb9906b028d828e812d29
CayduRover2 <caydenvandusen@gmail.com>
2026-09-14 02:19:46 -0400

    Update RoverApplication.kt

--- app/src/main/java/land/otter/roverd/RoverApplication.kt
+++ app/src/main/java/land/otter/roverd/RoverApplication.kt
@@ -1,11 +1,13 @@
 package land.otter.roverd
 
 import androidx.multidex.MultiDexApplication
+import com.serenegiant.utils.UVCUtils
 
 class RoverApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         RoverRuntimeState.initialize(this)
+        UVCUtils.init(this)
         CrashLogger.install(this)
         RoverAudioController.initialize(this)
         HeadlightController.initialize(this)
